281b866ad547aa9d6531b9370c634a917398b8a7
Pete Vilter <bonasaurus1@gmail.com>
2008-08-13 16:18:06 +0000

    some tweaks that were here... don't even remember...

--- app/config.php
+++ app/config.php
@@ -1,9 +1,9 @@
 <?php
 $config = array(
   'database' => 'database.sqlite3',
-  'base_url' => 'http://mac-mini.local/braindump/trunk/',
+  'base_url' => 'http://mac-mini.local/svn/braindump/trunk/',
   'clean_urls' => false,
   #'error_reporting' => E_ALL,
-  'keep_log' => false
+  'keep_log' => true
 );
 ?>
