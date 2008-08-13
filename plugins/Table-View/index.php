2ac751e229a0182ad775ba3ffdc500c13c13341c
Pete Vilter <bonasaurus1@gmail.com>
2008-08-13 16:40:07 +0000

    now table view doesn't throw error when it has no pages to display

--- plugins/Table-View/index.php
+++ plugins/Table-View/index.php
@@ -1,9 +1,11 @@
 <?php  
 $pages = array();
-foreach(Graph::_list($_GET['criteria']) as $page) {
-  $pages[$page] = Graph::get($page);
+$pagelist = Graph::_list($_GET['criteria']);
+if($pagelist) {
+  foreach($pagelist as $page) {
+    $pages[$page] = Graph::get($page);
+  }
 }
-
 $attributes = array();
 foreach($pages as $page) {
   if($page) {
@@ -27,23 +29,27 @@ $(document).ready(function(){
   <input type="text" name="criteria" value="<?php echo $_GET['criteria'] ?>"/>
   <input type="submit" value="Update"/>
 </form>
-<table class="tablesorter">
-  <thead>
-    <tr>
-      <th>Page Name</th>
-      <?php foreach($attributes as $attr): ?>
-        <th><?php echo $attr ?></th>
-      <?php endforeach ?>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach($pages as $name=>$data): ?>
+<?php if($pages): ?>
+  <table class="tablesorter">
+    <thead>
       <tr>
-        <td class="page_value"><?php echo page_link($name) ?></td>
+        <th>Page Name</th>
         <?php foreach($attributes as $attr): ?>
-          <td class="page_value"><?php print_or_list_with_link($data[$attr]) ?></td>
+          <th><?php echo $attr ?></th>
         <?php endforeach ?>
       </tr>
-    <?php endforeach ?>
-  </tbody>
-</table>
+    </thead>
+    <tbody>
+      <?php foreach($pages as $name=>$data): ?>
+        <tr>
+          <td class="page_value"><?php echo page_link($name) ?></td>
+          <?php foreach($attributes as $attr): ?>
+            <td class="page_value"><?php print_or_list_with_link($data[$attr]) ?></td>
+          <?php endforeach ?>
+        </tr>
+      <?php endforeach ?>
+    </tbody>
+  </table>
+<?php else: ?>
+  <p class="notice">No pages to display.</p>
+<?php endif ?>
