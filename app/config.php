2c5304af4055667831c264602f043a4c732ed052
Pete Vilter <bonasaurus1@gmail.com>
2008-10-08 19:13:19 +0000

    tagging 0.4

--- app/config.php
+++ app/config.php
@@ -1,7 +1,7 @@
 <?php
 $config = array(
   'database' => 'database.sqlite3',
-  'base_url' => 'http://mac-mini.local/svn/braindump/trunk/',
+  'base_url' => 'http://mac-mini.local/svn/braindump/tags/0.3/',
   'clean_urls' => false,
   #'error_reporting' => E_ALL,
   'keep_log' => true
